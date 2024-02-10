c5c6577dc21e41d24029b5a847c31731cfbbee26
Dinesh <somkuwardinesh@yahoo.com>
2024-02-10 22:03:53 +0530

    test

---
+++ src/main/java/com/apigtservice/configs/DevCorsConfig.java
@@ -0,0 +1,17 @@
+package com.apigtservice.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class DevCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://35.201.250.66")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+    }
+}
